ca96c035fcccdce35173c008b2884e4080abd2c2
jhonManuelGil <jhonmanuelg6@gmail.com>
2024-11-27 00:47:26 -0300

    subiendo pratica: estrutura de decisão

--- 1 clases Python Fundamentals/repaso_das_aulas.py
+++ 1 clases Python Fundamentals/repaso_das_aulas.py
@@ -13,7 +13,7 @@
 
 # Ejercicio 1
 
-
+print("#############-Ejercicio 1-########################")
 variavel_1 = "JhonGil"
 
 if variavel_1 == "JhonGil":
@@ -24,11 +24,51 @@ else:
    
     print("La variable no es igual a JhonGil 😢😢😢 \n")
    
-   
+print("___________________________________")
 num_1 = 25
 nome_2 = "karla"
 lista = ["luis", "lara", "jesus", "rober"]
 
 if num_1 != nome_2:
     print("El número es igual al nombre \n")
-   
+    
+
+print("#############-Ejercicio 2-########################")
+# Ejercicio 2
+name = "jhon"
+
+if name == "jhon t":
+ print("El nombre es jhon \n")
+elif name == "jhon ge":
+    print("El nombre es jhon ge ")
+else:
+    print("El nombre no es jhon \n")
+    
+print("___________________________________")
+
+x = 10
+resto= x % 25
+
+if resto == 0:
+ print("El número es par ")
+else:
+ print("El número es impar ")
+
+print("___________________________________")
+
+nota = 10
+
+if nota>= 9:
+    print(f"Aprobado con nota: {nota} muy buena felicidades")
+elif nota >= 8:
+    print(f"Aprobado con nota: {nota} buena mas tienes que meforar mas")
+elif nota >= 7:
+    print(f"Aprobado con nota: {nota} nedia")
+elif nota >= 6:
+    print(f"Aprobado con nota: {nota} muy baja tienes que mejorar")
+else:
+    print(f"Reprobado con nota: {nota} estuia mas")
+
+print("___________________________________")
+
+
