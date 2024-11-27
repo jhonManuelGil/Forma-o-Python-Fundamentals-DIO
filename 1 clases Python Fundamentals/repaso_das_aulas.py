829f60a9c789b5811e3e2d745d590ae59d572a4f
jhonManuelGil <jhonmanuelg6@gmail.com>
2024-11-26 23:01:53 -0300

    subiendo pratica de repaso

---
+++ 1 clases Python Fundamentals/repaso_das_aulas.py
@@ -0,0 +1,34 @@
+"""
+   Extruturas condional 
+   # if com  elif
+   
+   # if con else
+   
+   #for con if
+    
+   #while con if
+    
+    
+"""
+
+# Ejercicio 1
+
+
+variavel_1 = "JhonGil"
+
+if variavel_1 == "JhonGil":
+   
+    print("La variable es igual a JhonGil 💕💕💕 \n")
+    
+else:
+   
+    print("La variable no es igual a JhonGil 😢😢😢 \n")
+   
+   
+num_1 = 25
+nome_2 = "karla"
+lista = ["luis", "lara", "jesus", "rober"]
+
+if num_1 != nome_2:
+    print("El número es igual al nombre \n")
+   
