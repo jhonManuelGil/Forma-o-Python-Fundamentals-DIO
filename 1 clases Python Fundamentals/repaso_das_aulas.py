addb0b4a0f41c29656d6e8c9a38f5462a38a7c1e
jhonManuelGil <jhonmanuelg6@gmail.com>
2024-11-28 03:23:55 -0300

    Uploading: classroom review information Arithmetic operators Relational operators Bitwise operators Assignment operators Logical operators Membership operators Identity operators

--- 1 clases Python Fundamentals/repaso_das_aulas.py
+++ 1 clases Python Fundamentals/repaso_das_aulas.py
@@ -1,72 +1,166 @@
+# """
+#    Extruturas condional 
+#    # if com  elif
+   
+#    # if con else
+   
+#    #for con if
+    
+#    #while con if
+    
+    
+# """
+
+# # Ejercicio 1
+
+# print("#############-Ejercicio 1-########################")
+# variavel_1 = "JhonGil"
+
+# if variavel_1 == "JhonGil":
+   
+#     print("La variable es igual a JhonGil 💕💕💕 \n")
+    
+# else:
+   
+#     print("La variable no es igual a JhonGil 😢😢😢 \n")
+   
+# print("___________________________________")
+# num_1 = 25
+# nome_2 = "karla"
+# lista = ["luis", "lara", "jesus", "rober"]
+
+# if num_1 != nome_2:
+#     print("El número es igual al nombre \n")
+    
+
+# print("#############-Ejercicio 2-########################")
+# # Ejercicio 2
+# name = "jhon"
+
+# if name == "jhon t":
+#  print("El nombre es jhon \n")
+# elif name == "jhon ge":
+#     print("El nombre es jhon ge ")
+# else:
+#     print("El nombre no es jhon \n")
+    
+# print("___________________________________")
+
+# x = 10
+# resto= x % 25
+
+# if resto == 0:
+#  print("El número es par ")
+# else:
+#  print("El número es impar ")
+
+# print("___________________________________")
+
+# nota = 10
+
+# if nota>= 9:
+#     print(f"Aprobado con nota: {nota} muy buena felicidades")
+# elif nota >= 8:
+#     print(f"Aprobado con nota: {nota} buena mas tienes que meforar mas")
+# elif nota >= 7:
+#     print(f"Aprobado con nota: {nota} nedia")
+# elif nota >= 6:
+#     print(f"Aprobado con nota: {nota} muy baja tienes que mejorar")
+# else:
+#     print(f"Reprobado con nota: {nota} estuia mas")
+
+# print("___________________________________")
+
+
+# print("#############-Ejercicio 3-######################## \n")
+# # Ejercicio 3
+
+# numer = 12 # -5 El número NO es positivo / 5 El número SI es positivo
+
+# if numer > 0:
+#     print("El número SI es positivo")
+# elif numer < 0:
+#     print("El número NO es positivo \n")
+    
+# print("Fin de programa")
+
+# print("___________________________________ \n")
+
+# notas = float(input("Intraduce una nota: "))
+
+# if notas < 5:
+#     print(F"Reprobado con {notas} muy baja estudias mas")
+# elif notas > 5:
+#     print(f"Aprobado con {notas} felicidades")
+
+# and
+# print("___________________________________ \n")
+
+# nota = float(input("Introduce su nota: "))
+
+# if 0 <= nota and nota < 5:
+#     print(f"Reprobado con {nota} esta en el limite estudia mas")
+# elif 5 <= nota and nota < 7:
+#     print(f"Aprobado con {nota} nota en rango normal")
+# elif  5 <= nota and nota < 8:
+#     print(f"Aprobado con {nota} buena nota")
+# elif 5 <= nota and nota < 9:
+#     print(f"Aprobado con {nota} muy buena nota")
+# else:
+#     print(f"Aprobado con {nota} nota muy buena felicidade")
+
+
+# print("___________________________________ \n")
+
+# n1 = int(input("Digite un número: "))
+
+# if n1 > 50:
+#     print(f"o numero {n1} é maior que 50")
+# else:
+#     print(f"o numero {n1} é menor que 50")
+
+
+# print("___________________________________ \n")
+
+
+# num1 = int(input("Digite un numero:  "))
+
+# if num1 % 2 == 0:
+#     print(f"O numero {num1} é impar")
+# else:
+#     print(f"O numero {num1} é par")
+
+
+
+
+
+print("___________________________________ \n")
+
+idade = int(input("Intraduce sua idade: "))
+
+if idade > 17:
+    print(f"Você tem {idade} anos e es maior edade")
+elif idade < 18:   print(f"Você tem {idade} anos e é menor de idade")
+
 """
-   Extruturas condional 
-   # if com  elif
-   
-   # if con else
-   
-   #for con if
+    Los operadores son símbolos que le indican al intérprete que realice una 
+    operación específica, como aritmética,  comparación, lógica, etc
     
-   #while con if
-    
-    
+Operadores aritméticos
+Operadores relacionales
+Operadores Bit a Bit
+Operadores de asignación
+Operadores lógicos
+Operadores de pertenencia
+Operadores de identidad
+
 """
 
-# Ejercicio 1
+print("#############-Ejercicio 4-######################## \n")
+# # Ejercicio 4
+print("___________________________________ \n")
 
-print("#############-Ejercicio 1-########################")
-variavel_1 = "JhonGil"
 
-if variavel_1 == "JhonGil":
-   
-    print("La variable es igual a JhonGil 💕💕💕 \n")
-    
-else:
-   
-    print("La variable no es igual a JhonGil 😢😢😢 \n")
-   
-print("___________________________________")
-num_1 = 25
-nome_2 = "karla"
-lista = ["luis", "lara", "jesus", "rober"]
 
-if num_1 != nome_2:
-    print("El número es igual al nombre \n")
-    
 
-print("#############-Ejercicio 2-########################")
-# Ejercicio 2
-name = "jhon"
-
-if name == "jhon t":
- print("El nombre es jhon \n")
-elif name == "jhon ge":
-    print("El nombre es jhon ge ")
-else:
-    print("El nombre no es jhon \n")
-    
-print("___________________________________")
-
-x = 10
-resto= x % 25
-
-if resto == 0:
- print("El número es par ")
-else:
- print("El número es impar ")
-
-print("___________________________________")
-
-nota = 10
-
-if nota>= 9:
-    print(f"Aprobado con nota: {nota} muy buena felicidades")
-elif nota >= 8:
-    print(f"Aprobado con nota: {nota} buena mas tienes que meforar mas")
-elif nota >= 7:
-    print(f"Aprobado con nota: {nota} nedia")
-elif nota >= 6:
-    print(f"Aprobado con nota: {nota} muy baja tienes que mejorar")
-else:
-    print(f"Reprobado con nota: {nota} estuia mas")
-
-print("___________________________________")
+print("___________________________________ \n")
